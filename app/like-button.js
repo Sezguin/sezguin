7f5e3429db29b204307a78562bc257fdd03a6257
Sam Pepper <sezguin@outlook.com>
2024-04-02 23:56:16 +0100

    :sparkles: Added basic code and next.js setup.

---
+++ app/like-button.js
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+`;
+
+export default function LikeButton() {
+    const [likes, setLikes] = useState(0);
+
+    function handleClick() {
+        setLikes(likes + 1);
+      }
+
+      return <StyledButton onClick={handleClick}>Like ({likes})</StyledButton>
+}
